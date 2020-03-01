6631f1e2e29af7cc88a8b8980b750c4615d2ce63
MasatakaKudou <b1017027@fun.ac.jp>
2020-03-02 00:00:15 +0900

    [add]1桁にも対応した

--- lib/bingo.rb
+++ lib/bingo.rb
@@ -3,24 +3,33 @@ class Bingo
     b = (1..15).to_a.sample(5)
     i = (16..30).to_a.sample(5)
     n = (31..45).to_a.sample(5)
-    n[2] = ""
     g = (46..60).to_a.sample(5)
     o = (61..75).to_a.sample(5)
     bingo = " B |  I |  N |  G |  O\n"
     5.times do |number|
       [b,i,n,g,o].each do |column|
-        bingo += column[number].to_s.rjust(2) + " | "
+        if column[number].to_s.length == 1
+          column[number] = "%02d" % column[number]
+        end
+        if number == 2 && column == n
+          bingo += "  " + " | "
+        else
+          bingo += column[number].to_s + " | "
+        end
       end
       bingo[-3..-1]="\n"
     end
     return bingo
   end
   def self.delete_number(bingo)
-    random = rand(1..75).to_s.rjust(2)
+    random = rand(1..75).to_s
+    if random.length == 1
+      random = "%02d" % random
+    end
     puts "今回は#{random}番です。"
     if /#{random}/ =~ bingo
       puts "あなたのカードに#{random}番ありました。"
-      bingo.gsub(/#{random}/, '')
+      bingo.sub!(/#{random}/, '  ')
     else
       puts "あなたのカードに#{random}番ありませんでした。"
     end
@@ -30,8 +39,8 @@ end
 my_bingo = Bingo.make_bingo
 puts 'こちらがあなたのビンゴカードです。'
 puts my_bingo
-print 'ビンゴを始めますか？(y/n): '
-answer = gets.chomp
+print 'ビンゴしますか？(y/n): '
+answer = gets.chomp 
 case answer
 when 'y'
   Bingo.delete_number(my_bingo)
